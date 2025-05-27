2449c4d2ece0f8654a9fbe71c021e89029198107
rupali <rupalinajan123>
2025-05-27 14:56:48 +0530

    Added new files

---
+++ config/mail.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    'default' => env('MAIL_MAILER', 'smtp'),
+    'mailers' => [
+        'smtp' => [
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'port' => env('MAIL_PORT', 465),
+            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'auth_mode' => null,
+            'stream' => [
+                'ssl' => [
+                    'verify_peer' => false,  // Disable peer verification
+                    'verify_peer_name' => false,  // Disable peer name verification
+                    'allow_self_signed' => true,  // Allow self-signed certificates
+                ],
+            ],
+        ],
+    ],
+    'from' => [
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
+    ],
+];
+
+
+?>
